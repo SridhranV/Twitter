b470f1b608c71af74cd0d79d965ce24d789388d4
SridhranV <119745281+SridhranV@users.noreply.github.com>
2022-12-03 22:21:44 +0530

    Add files via upload

---
+++ TestSample.java
@@ -0,0 +1,26 @@
+package org.git.GitProject;
+
+public class TestSample
+{
+	private void t1()
+	{
+		System.out.println("1");
+	}
+	private void t2()
+	{
+		System.out.println("4");
+	}
+	private void t3()
+	{
+		System.out.println("3");
+	}
+	
+	public static void main(String[] args)
+	{
+		TestSample ts = new TestSample();
+		ts.t1();
+		ts.t2();
+		ts.t3();
+	}
+
+}
